b094e45d7470c2b1937631b6604560f57d1a34ba
AdrianGR55 <19aroman@cco.ie>
2025-01-18 17:22:38 +0000

    Data Cleaning

--- getData.py
+++ getData.py
@@ -1,18 +1,25 @@
 
+import json
 import pandas as pd
 
 
-file_path = 'HistoricalDataSP500.csv'
-df = pd.read_csv(file_path)
-
-df_head = df.head()
-df_info = df.info()
-df_nulls = df.isnull().sum()
-
-df = df.dropna()
+with open("HistoricalDataSP500.json", "r") as f:
+    data = json.load(f)
 
 
-cleaned_data = 'CleanHistoricalDataSP500.csv'
-df.to_csv(cleaned_data, index=False)
+ts = data["chart"]["result"][0]["timestamp"];
+o = data["chart"]["result"][0]["indicators"]["quote"][0]["open"];
+h = data["chart"]["result"][0]["indicators"]["quote"][0]["high"];
+l = data["chart"]["result"][0]["indicators"]["quote"][0]["low"];
+c = data["chart"]["result"][0]["indicators"]["quote"][0]["close"];
 
+dt = pd.to_datetime(ts, unit="s")
+
+
+df = pd.DataFrame({"Date": dt, "Open": o, "High": h, "Low": l, "Close": c})
+df = df[(df["Date"].dt.year != 1927) & (df["Date"].dt.year != 2025)] 
+df["Date"] = df["Date"].dt.date
+
+
+df.to_csv("HistoricalDataSP500.csv", index=False)
 
